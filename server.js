6b4ff3a08580aec181c0c7e3854bc40a9a6651c9
Rafael Garrafiel <rafaelgarrafiel@gmail.com>
2022-05-05 10:12:36 -0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const path = require("path");
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.use(express.static('build'))
+app.use(express.static('dist'))
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
